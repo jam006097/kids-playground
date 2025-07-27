18b4c6cde0e76f1e505c2120d08f253a25bcd027
jam <jam006097@gmail.com>
2025-07-27 15:47:01 +0900

    管理者ページにDBの各テーブルが表示されるように変更

--- myapp/admin.py
+++ myapp/admin.py
@@ -1,3 +1,7 @@
-# from django.contrib import admin
+from django.contrib import admin
+from .models import Playground, Favorite, Review
 
 # Register your models here.
+admin.site.register(Playground)
+admin.site.register(Favorite)
+admin.site.register(Review)

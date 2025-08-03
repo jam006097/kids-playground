8d1c35b8636d6d0607d3d0e632745c8881011858
jam <jam006097@gmail.com>
2025-08-03 16:45:58 +0900

    テストコードの整理

--- myapp/tests/test_views_playground.py
+++ myapp/tests/test_views_playground.py
@@ -21,7 +21,6 @@ class PlaygroundListViewTest(TestCase):
         self.assertEqual(response.status_code, 200)
         self.assertTemplateUsed(response, "playgrounds/list.html")
         self.assertEqual(len(response.context["playgrounds"]), 2)
-        self.assertNotContains(response, 'id="mypage-tab"')
 
     # 都市フィルタ付きで公園リストビューが正常に表示されることをテスト
     def test_playground_list_view_with_city_filter(self):

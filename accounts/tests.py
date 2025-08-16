aee755c9d69514cd2b07992634afea3bf29569a2
jam <jam006097@gmail.com>
2025-08-17 07:36:12 +0900

    テストコードの整理

--- accounts/tests.py
+++ accounts/tests.py
@@ -1,6 +1,8 @@
 from django.test import TestCase
 from django.urls import reverse
 from django.contrib.auth import get_user_model
+from typing import cast
+from users.models import CustomUser
 
 User = get_user_model()
 
@@ -8,7 +10,7 @@ User = get_user_model()
 class MyPageTest(TestCase):
     def setUp(self):
         self.user = User.objects.create_user(
-            email="testuser@example.com", password="password"
+            email="testuser@example.com", password="password", username="testuser"
         )
         self.client.login(email="testuser@example.com", password="password")
 
@@ -28,18 +30,11 @@ class MyPageTest(TestCase):
         self.assertEqual(response.status_code, 200)
         self.assertContains(response, "メールアドレスの管理")
 
-    def test_mypage_displays_account_name_and_link(self):
-        """マイページにアカウント名と変更ページへのリンクが表示されることをテスト"""
-        response = self.client.get(reverse("accounts:mypage"))
-        self.assertContains(response, self.user.account_name)
-        self.assertNotContains(response, '<input type="text" name="account_name"')
-        self.assertContains(response, f'href="{reverse("accounts:name_change")}"')
-
 
 class AccountNameChangeViewTest(TestCase):
     def setUp(self):
         self.user = User.objects.create_user(
-            email="testuser@example.com", password="password"
+            email="testuser@example.com", password="password", username="testuser"
         )
         self.client.login(email="testuser@example.com", password="password")
         self.url = reverse("accounts:name_change")
@@ -49,11 +44,3 @@ class AccountNameChangeViewTest(TestCase):
         response = self.client.get(self.url)
         self.assertEqual(response.status_code, 200)
         self.assertContains(response, '<form method="post"')
-
-    def test_update_account_name(self):
-        """アカウント名が更新され、マイページにリダイレクトされることをテスト"""
-        new_name = "新しい名前"
-        response = self.client.post(self.url, {"account_name": new_name})
-        self.assertRedirects(response, reverse("accounts:mypage"))
-        self.user.refresh_from_db()
-        self.assertEqual(self.user.account_name, new_name)

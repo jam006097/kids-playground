b8738d9fda9289d4359e107fc4b67c8a32e67dd6
jam <jam006097@gmail.com>
2025-08-17 08:14:07 +0900

    utilsへコメントの追加

--- myapp/static/js/utils.js
+++ myapp/static/js/utils.js
@@ -1,9 +1,16 @@
+/**
+ * 指定された名前のクッキーの値を取得します。
+ *
+ * @param {string} name - 取得するクッキーの名前。
+ * @returns {string|null} 指定された名前のクッキーの値。クッキーが存在しない場合はnull。
+ */
 export function getCookie(name) {
   let cookieValue = null;
   if (document.cookie && document.cookie !== '') {
     const cookies = document.cookie.split(';');
     for (let i = 0; i < cookies.length; i++) {
       const cookie = cookies[i].trim();
+      // このクッキー文字列が探している名前で始まるかチェック
       if (cookie.substring(0, name.length + 1) === name + '=') {
         cookieValue = decodeURIComponent(cookie.substring(name.length + 1));
         break;

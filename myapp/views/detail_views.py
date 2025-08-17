d1e715a46e973a1bc525c79f3313e7313d767396
jam <jam006097@gmail.com>
2025-08-17 10:55:53 +0900

    コメントの追加

--- myapp/views/detail_views.py
+++ myapp/views/detail_views.py
@@ -3,8 +3,12 @@ from myapp.models import Playground
 
 
 class FacilityDetailView(DetailView):
-    model = Playground
-    template_name = "myapp/facility_detail.html"
-    context_object_name = (
-        "playground"  # This makes the object available as 'playground' in the template
-    )
+    """
+    施設詳細ビュー
+
+    指定されたPlaygroundオブジェクトの詳細を表示します。
+    """
+
+    model = Playground  # このビューで使用するモデルをPlaygroundに設定
+    template_name = "myapp/facility_detail.html"  # 使用するテンプレートを指定
+    context_object_name = "playground"  # テンプレート内でオブジェクトにアクセスするための変数名を'playground'に設定

a18fb6353b74857cb633b486fcc705d522b8119b
jam <jam006097@gmail.com>
2025-09-14 17:43:00 +0900

    jqueryのモックを削除

--- jest.config.js
+++ jest.config.js
@@ -5,7 +5,6 @@ module.exports = {
   },
   moduleNameMapper: {
     '^leaflet$': '<rootDir>/__mocks__/leafletMock.js',
-    '^jquery$': '<rootDir>/__mocks__/jqueryMock.js',
     '^../myapp/static/js/utils.js$': '<rootDir>/myapp/static/js/utils.js',
     '^@mapManager$': '<rootDir>/myapp/static/js/map.js',
   },

a1a14f45ca55c73ba7c6bf54f27638be77255a0d
jam <jam006097@gmail.com>
2025-09-20 05:09:05 +0900

    コードの整理

--- myapp/views/chat_views.py
+++ myapp/views/chat_views.py
@@ -2,7 +2,7 @@ from django.views.generic import TemplateView
 
 
 class ChatView(TemplateView):
-    template_name = "myapp/chat.html"
+    template_name = "chatbot/chat.html"
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
